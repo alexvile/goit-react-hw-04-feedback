aa40814690501a92b11aab1295c80bb52cc0242e
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-09-27 20:08:24 +0300

    copied files from 02-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import PropTypes from 'prop-types';
+import React from 'react';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import Section from './Section/Section';
+import { StatisticsTitle } from './App.styled';
+
+class App extends React.Component {
+  // static defaultProps = {
+  //   initialValue: 0,
+  // };
+  // static propTypes = {
+  //   initialValue: PropTypes.number,
+  // };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  getState = () => {
+    const states = Object.keys(this.state);
+    return states;
+  };
+  leaveFeedback = objKey => {
+    this.setState(prevState => ({
+      [objKey]: prevState[objKey] + 1,
+    }));
+  };
+
+  render() {
+    let countTotalFeedback =
+      this.state.good + this.state.neutral + this.state.bad;
+    let countPositiveFeedbackPercentage = Math.round(
+      (this.state.good / countTotalFeedback) * 100
+    );
+
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={this.getState()}
+          onLeaveFeedback={this.leaveFeedback}
+        />
+        <StatisticsTitle>Statistics</StatisticsTitle>
+        {countTotalFeedback === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={countTotalFeedback}
+            positivePercentage={countPositiveFeedbackPercentage}
+          />
+        )}
+      </Section>
+    );
+  }
+}
+export default App;
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const StatisticsTitle = styled.h2`
+  color: #000;
+  font-family: 'Lato', sans-serif;
+  font-size: 36px;
+  font-weight: 300;
+  line-height: 1.2;
+  margin-bottom: 15px;
+`;
